34d492838f08202f408753c2c030a33b80109f50
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2024-04-17 23:26:01 +0200

    Basic layout for toggling book buttons

--- phase 3/JavaScripts/buttonScript.js
+++ phase 3/JavaScripts/buttonScript.js
@@ -1,6 +1,81 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+function getUserType() {
+    const isAdminElement = document.getElementById("is_admin");
+    if (isAdminElement) {
+        const isAdmin = isAdminElement.value;
+        if (isAdmin === "1") {
+            userType = 'admin';
+            localStorage.setItem('userType', 'admin'); 
+            window.location.href = "adminsview.html";
+        } else if (isAdmin === "0") {
+            userType = 'user';
+            localStorage.setItem('userType', 'user'); 
+            window.location.href = "usersView.html";
+        } else {
+            userType = 'default';
+        }
+    } else {
+        userType = 'default';
+    }
+}
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+function getUserTypeFromStorage() {
+    return localStorage.getItem('userType');
+}
+
+function clearUserType() {
+    localStorage.removeItem('userType'); 
+}
+
+function logout() {
+    clearUserType();
+    window.location.href = "defaultview.html";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementsByClassName("toggle-button")[0];
+    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+    toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active')
+    })
+
+    function updateNavigationBar() {
+        const userType = getUserTypeFromStorage(); 
+        console.log(userType);
+        if (userType === 'admin') {
+            navbarLinks.innerHTML = `
+                <ul>
+                    <li><a href="Profile.html?userType=admin">Profile</a></li>
+                    <li><a href="adminsview.html?userType=admin">Home</a></li>
+                    <li><a href="search.html?userType=admin">Search</a></li>
+                    <li><a href="addBook.html?userType=admin">Add Book</a></li>
+                    <li><a href="#" onclick="logout()">Logout</a></li> <!-- Add onclick event for logout -->
+                </ul>
+            `;
+        } else if (userType === 'user') {
+            navbarLinks.innerHTML = `
+                <ul>
+                    <li><a href="Profile.html?userType=user">Profile</a></li>
+                    <li><a href="usersView.html?userType=user">Home</a></li>
+                    <li><a href="search.html?userType=user">Search</a></li>
+                    <li><a href="Borrowed Books.html?userType=user">Borrow Books</a></li>
+                    <li><a href="#" onclick="logout()">Logout</a></li> <!-- Add onclick event for logout -->
+                </ul>
+            `;
+        } else {
+            
+            navbarLinks.innerHTML = `
+                <ul>
+                    <li><a href="defaultview.html">Home</a></li>
+                    <li><a href="search.html">Search</a></li>
+                    <li><a href="Book Listing.html">View Books</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="signup.html">Sign Up</a></li>
+                </ul>
+            `;
+        }
+        
+    }
+
+    updateNavigationBar();
+});
