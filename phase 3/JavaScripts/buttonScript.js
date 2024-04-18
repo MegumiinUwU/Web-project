c4bd80502547e94a7c569d8047645e4cc642cecf
MegumiinUwU <youssefmohamed81222@gmail.com>
2024-04-18 10:29:35 +0200

    Nada deleted extra button

--- phase 3/JavaScripts/buttonScript.js
+++ phase 3/JavaScripts/buttonScript.js
@@ -35,9 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementsByClassName("toggle-button")[0];
     const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
-    })
 
     toggleButton.addEventListener('click', () => {
         navbarLinks.classList.toggle('active');
