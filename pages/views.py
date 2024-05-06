00e8cc95514fbefa834baa07defb668706cfd6fd
Mazen-Abdelfattah <mazentomas@gmail.com>
2024-05-06 23:29:03 +0300

    This is the django files that you will need

---
+++ pages/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+
+
+def bookListing(request):
+    return render(request, 'pages/Book Listing.html')
+
+
+def bookList(request):
+    return render(request, 'pages/BookList.html')
+
+
+def borrowBooks(request):
+    return render(request, 'pages/BorrowBooks.html')
+
+
+def borrowedBooks(request):
+    return render(request, 'pages/Borrowed Books.html')
+
+
+def homePage(request):
+    return render(request, 'pages/HomePage.html') 
+
+
+def inventory(request):
+    return render(request, 'pages/Inventory.html')
+
+
+def profile(request):
+    return render(request, 'pages/Profile.html')
+
+
+def addBook(request):
+    return render(request, 'pages/addBook.html')
+
+
+def login(request):
+    return render(request,'pages/login.html')
+
+
+def search(request):
+    return render(request, 'pages/search.html')
+
+
+def signup(request):
+    return render(request, 'pages/signup.html')
+
+
+def wishlist(request):
+    return render(request, 'pages/wishlist.html')
