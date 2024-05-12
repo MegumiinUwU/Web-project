ea36e395415a362dafc5e5834e96061de82a0312
Ronii55 <roniibassiouny@gmail.com>
2024-05-12 21:48:56 +0300

    finish all profile functions

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+     image=models.ImageField(upload_to="photos/%y/%m/%d" , default="PfP.jpg")
+     username = models.CharField(max_length=50)
+     password = models.CharField(max_length=50)
+     email = models.CharField(max_length=50, default="user@gmail.com")
+     phoneNumber=models.IntegerField(default=111111111111)
+     date = models.DateField(null=True,blank=True)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -32,8 +35,34 @@ def inventory(request):
 
 
 def profile(request):
-    return render(request, 'pages/Profile.html')
+    profile=Profile.objects.get(username='youesf')
+    if request.method == 'POST':
+        old_password = request.POST.get('oldpassword')
+        new_password = request.POST.get('newpassword')
+        confirm_password = request.POST.get('confirmpassword')
+     
 
+        if old_password == profile.password:
+            profile_picture = request.FILES.get('profilePicture')
+            if profile_picture:
+                profile.image.save(profile_picture.name, profile_picture)
+            
+            if new_password == confirm_password:
+                 if new_password:
+                    profile.password = new_password
+                    profile.username = request.POST.get('username')
+                    profile.email = request.POST.get('email')
+                    profile.phoneNumber = request.POST.get('phoneNumber')
+                    profile.date = request.POST.get('dateOfBirth')
+                    profile.save()
+            else:
+                messages.error(request, 'New password and confirmation do not match.')
+        
+        else:
+            messages.error(request, 'The password you entered is incorrect.')
+  
+    return render(request, 'pages/Profile.html',{"profile":profile})
+ 
 
 def addBook(request):
     return render(request, 'pages/addBook.html')
